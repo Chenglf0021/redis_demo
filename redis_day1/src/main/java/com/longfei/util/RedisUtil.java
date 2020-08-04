e3a9a87b6828d87057c9b39370a4f64f68f2ca92
e <1976411121@qq.com>
2020-08-04 11:52:06 +0800

    redis_demo

--- redis_day1/src/main/java/com/longfei/util/RedisUtil.java
+++ redis_day1/src/main/java/com/longfei/util/RedisUtil.java
@@ -1,15 +1,50 @@
 package com.longfei.util;
 
 import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class RedisUtil {
+    /**
+     * @简单模式
+     */
     @Test
     public void testConnectionRedisSimple() {
         Jedis jedis = new Jedis("106.54.242.90", 6379);
-        jedis.set("test","1111111111");
+        jedis.set("test", "1111111111");
         String result = jedis.get("test");
         System.out.println(result);
+        jedis.close();
     }
 
+    /**
+     * @Desc:连接池
+     */
+    @Test
+    public void testConnectRedisByPool() {
+        JedisPool jedisPool = new JedisPool("106.54.242.90", 6379);
+        Jedis jedis = jedisPool.getResource();
+        jedis.get("test");
+        jedis.close();
+        jedisPool.close();
+    }
+
+    /**
+     * @Desc:集群
+     */
+    @Test
+    public void testConnectRedisFactory(){
+        Set<HostAndPort> hostAndPortSet=new HashSet<HostAndPort>();
+        hostAndPortSet.add(new HostAndPort("106.54.242.90",6379));
+        hostAndPortSet.add(new HostAndPort("",6379));
+        JedisCluster jedisCluster=new JedisCluster(hostAndPortSet);
+        String result=jedisCluster.get("test");
+        System.out.println(result);
+        jedisCluster.close();
+    }
 }
