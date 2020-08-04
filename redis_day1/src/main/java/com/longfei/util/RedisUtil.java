270090808f3648ba77b3c0ba019353108ed00972
e <1976411121@qq.com>
2020-08-04 11:41:16 +0800

    redis_demo

--- redis_day1/src/main/java/com/longfei/util/RedisUtil.java
+++ redis_day1/src/main/java/com/longfei/util/RedisUtil.java
@@ -1,4 +1,15 @@
 package com.longfei.util;
 
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
 public class RedisUtil {
+    @Test
+    public void testConnectionRedisSimple() {
+        Jedis jedis = new Jedis("106.54.242.90", 6379);
+        jedis.set("test","1111111111");
+        String result = jedis.get("test");
+        System.out.println(result);
+    }
+
 }
